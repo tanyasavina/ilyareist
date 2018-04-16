4e034045554abf99d077ba32fdf18a63e70b1c7d
Ilia <i.feoktistov@memsik.ru>
2018-04-16 21:39:14 +0300

    war file

--- src/main/java/ru/programm/SuterApp.java
+++ src/main/java/ru/programm/SuterApp.java
@@ -1,13 +1,22 @@
 package ru.programm;
 
+//import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SuterApp {
+public class SuterApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SuterApp.class, args);
     }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SuterApp.class);
+    }
 
 }
+
+
